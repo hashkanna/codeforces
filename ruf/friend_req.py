e26817f734ece15785de94dcfc0e1a51e3051fa7
Kannappan Sirchabesan <hashkanna@gmail.com>
2018-03-17 09:48:06 +0000

    fix logic

--- ruf/friend_req.py
+++ ruf/friend_req.py
@@ -8,20 +8,22 @@ class Solution:
         count=0
         n=len(ages)
         for i in range(n):
-            for j in range(i+1,n):
-                if ages[j] <= (ages[i]/2)+7:
-                    print("1111")
-                    break
-                elif ages[j]>ages[i]:
-                    print("2222")
-                    break
-                elif ages[j] < 100 and ages[i] > 100:
-                    print("3333")
-                    break
-                else:
-                    count+=1
+            for j in range(n):
+                if i!=j:
+                    if ages[j] <= (ages[i]//2)+7:
+                        #print("1111")
+                        pass
+                    elif ages[j]>ages[i]:
+                        # print("2222")
+                        pass
+                    elif ages[j] < 100 and ages[i] > 100:
+                        #print("3333")
+                        pass
+                    else:
+                        count+=1
+                        print(ages[i], ages[j])
         return count
 
 sol=Solution()
-ages=[10,39,50]
+ages=[101,79,102]
 print(sol.friendRequest(ages=ages))
